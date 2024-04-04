b7a48a3859e0795ebf1273f71f82da352d243d25
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-04-03 23:22:12 -0300

    Add files via upload

---
+++ Lista1ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da aresta do quadrado: ");
+            int aresta = Convert.ToInt32(Console.ReadLine());
+
+            int area = aresta * aresta;
+
+            Console.WriteLine("A área do quadrado com aresta " + aresta + " é: " + area);
+        }
+    }
+}
