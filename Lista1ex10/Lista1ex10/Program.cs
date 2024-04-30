c630bf893d1e047d1874caeabe5047bfc275d8ff
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-04-30 13:40:55 -0300

    Add files via upload

---
+++ Lista1ex10/Lista1ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar ao usuário que insira a cotação do dólar
+        Console.Write("Digite a cotação do dólar: ");
+        double cotacaoDolar = Convert.ToDouble(Console.ReadLine());
+
+        // Solicitar ao usuário que insira a quantidade de dólares
+        Console.Write("Digite a quantidade de dólares: ");
+        double quantidadeDolares = Convert.ToDouble(Console.ReadLine());
+
+        // Calcular o valor em Reais
+        double valorReais = quantidadeDolares * cotacaoDolar;
+
+        // Exibir o resultado
+        Console.WriteLine($"O valor correspondente em Reais é: R$ {valorReais}");
+    }
+}
