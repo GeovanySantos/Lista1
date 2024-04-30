fef778cede007a747c9b8ca98ffff306834b6850
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-04-30 13:29:40 -0300

    Add files via upload

---
+++ Lista1ex7/Lista1ex7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Definir a constante para a equivalência entre milhas marítimas e metros
+        const double milhaMaritimaEmMetros = 1852;
+
+        // Definir a constante para a equivalência entre quilômetros e metros
+        const double quilometroEmMetros = 1000;
+
+        // Solicitar ao usuário que insira a quantidade de milhas marítimas
+        Console.Write("Digite a quantidade de milhas marítimas: ");
+        double milhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+        // Converter milhas marítimas para quilômetros
+        double quilometros = (milhasMaritimas * milhaMaritimaEmMetros) / quilometroEmMetros;
+
+        // Exibir o resultado
+        Console.WriteLine($"O equivalente de {milhasMaritimas} milhas marítimas em quilômetros é: {quilometros} km");
+    }
+}
