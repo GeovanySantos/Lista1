5c3eb4ce8a888b3ba02a9d0e648952d4c4d99d1b
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-04-30 13:45:34 -0300

    Add files via upload

---
+++ Lista1ex11/Lista1ex11/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar ao usuário que insira os valores de X e Y
+        Console.Write("Digite o valor de X: ");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite o valor de Y: ");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        // Calcular X elevado a Y
+        double resultado = Math.Pow(x, y);
+
+        // Exibir o resultado
+        Console.WriteLine($"O resultado de {x} elevado a {y} é: {resultado}");
+    }
+}
