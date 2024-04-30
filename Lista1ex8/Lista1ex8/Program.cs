0a962dfd9e40c98824e8c80e874e785cd18e0d01
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-04-30 13:32:56 -0300

    Add files via upload

---
+++ Lista1ex8/Lista1ex8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicitar ao usuário que insira a temperatura em graus Celsius
+        Console.Write("Digite a temperatura em graus Celsius: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        // Converter temperatura de Celsius para Fahrenheit
+        double fahrenheit = (celsius * 9 / 5) + 32;
+
+        // Exibir o resultado
+        Console.WriteLine($"A temperatura equivalente em Fahrenheit é: {fahrenheit} °F");
+    }
+}
