912c6c863571b59b600cbca6b12be82d83828ce4
GeovanySantos <165846992+GeovanySantos@users.noreply.github.com>
2024-12-12 20:37:57 -0300

    Update Program.cs

--- Lista1ex1/Program.cs
+++ Lista1ex1/Program.cs
@@ -1,5 +1,9 @@
 private void btnSemaforo1Verde_Click(object sender, EventArgs e)
 {
+    // Defina as imagens visíveis de acordo com o estado
+    pbSemaforo1Verde.Visible = true;
+    pbSemaforo1Amarelo.Visible = false;
+    pbSemaforo1Vermelho.Visible = false;
     semaforo1 = 0b00000001; // Ativa a luz verde no semáforo 1
-    AtualizarInterface();  // Atualiza a interface com os novos estados
+    AtualizarInterface();  // Atualiza a interface
 }
